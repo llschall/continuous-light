5e74429c50870fa5b3d0b48da9956033d7abaa84
llschall <llschall@gmx.com>
2026-01-03 18:00:41 +0100

    [Ribbon] Colors for each status

--- app/src/main/kotlin/org/llschall/continuous/light/request/Status.kt
+++ app/src/main/kotlin/org/llschall/continuous/light/request/Status.kt
@@ -4,14 +4,16 @@ import java.awt.Color
 
 enum class Status(val color: Color) {
 
-    COMPLETED(Color.green),
+    QUEUED(Color.orange),
     IN_PROGRESS(Color.yellow),
+    COMPLETED(Color.green),
     UNKNOWN(Color.red);
 
     fun fromString(value: String): Status {
         return when (value.lowercase()) {
-            "completed" -> COMPLETED
+            "queued" -> QUEUED
             "in_progress" -> IN_PROGRESS
+            "completed" -> COMPLETED
             else -> UNKNOWN
         }
     }
